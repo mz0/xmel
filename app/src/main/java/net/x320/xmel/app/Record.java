a3e27c7c0642810fa79891cfd9073c9339bc00fa
Mark Zhitomirski <mz@exactpro.com>
2021-08-11 22:24:30 +0300

    add simple Java StAX scanner

--- app/src/main/java/net/x320/xmel/app/Record.java
+++ app/src/main/java/net/x320/xmel/app/Record.java
@@ -1,2 +1,15 @@
-package net.x320.xmel.app;public class Record {
+package net.x320.xmel.app;
+
+import java.util.List;
+
+public class Record {
+    public final String id;
+    public final String name;
+    public final List<RecordField> fields;
+
+    public Record(String id, String name, List<RecordField> fieldList) {
+        this.id = id;
+        this.name = name;
+        this.fields = fieldList;
+    }
 }
--- app/src/main/kotlin/net/x320/xmel/app/App.kt
+++ app/src/main/kotlin/net/x320/xmel/app/App.kt
@@ -9,6 +9,24 @@ import javax.xml.stream.XMLStreamException
 
 fun main() {
     val tokens = StringUtils.split(MessageUtils.getMessage())
-    val result = StringUtils.join(tokens)
-    println(WordUtils.capitalize(result))
+    val result = WordUtils.capitalize(StringUtils.join(tokens))
+    val fileName =  "/home/mz0/e/smeh/in/mitch_TP_m2.xml"
+    val xmel = XmelReader()
+    try {
+        val results = xmel.read(fileName)
+    } catch (e: IOException) {
+        error(e.message.toString())
+    } catch (e: XMLStreamException) {
+        error(e.message.toString())
+    }
+    println("\nShared field definitions in Dictionary file $fileName:")
+    xmel.sharedFields.forEach { f ->
+        if (f.usage == 0) {
+            println("${f.id} is never used")
+        } else if (f.usage == 1) {
+            println("${f.id} is used only once")
+        } else {
+            println("${f.id} is used ${f.usage} times")
+        }
+    }
 }
